e2dd207c0289812e53bafba77b286c485c4abffb
Rika Ciminieri <rikaciminieri@gmail.com>
2021-09-15 19:46:12 -0700

    Middleware written

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,11 +1,50 @@
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+const Accounts = require("./accounts-model");
+const db = require("../../data/db-config");
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+exports.checkAccountPayload = (req, res, next) => {
+  const { name, budget } = req.body;
+  if (!name && !budget) {
+    next({ status: 400, message: "name and budget are required" });
+  }
+  if (typeof name !== "string") {
+    next({ status: 400, message: "name of account must be a string" });
+  }
+  const trimmedName = name.trim().length;
+  if (trimmedName < 3 || trimmedName > 100) {
+    next({ status: 400, message: "name of account must be between 3 and 100" });
+  }
+  if (typeof budget !== "number") {
+    next({ status: 400, message: "budget of account muse be a number" });
+  }
+  if (budget < 0 || budget > 1000000) {
+    next({
+      status: 400,
+      message: "budget of account is too large or too small",
+    });
+  }
+  next();
+};
+
+exports.checkAccountNameUnique = async (req, res, next) => {
+  const existing = await db("account")
+    .where("name", req.body.name.trim())
+    .first();
+  if (existing) {
+    next({ status: 400, message: "that name is taken" });
+  } else {
+    next()
+  }
+};
 
 exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+  Accounts.getById(req.params.id)
+    .then((account) => {
+      if (account) {
+        req.account = account;
+        next();
+      } else {
+        next({ status: 404, message: "account not found" });
+      }
+    })
+    .catch(next);
+};
--- api/server.js
+++ api/server.js
@@ -4,4 +4,15 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+  res.send('<h1>DATABASE PROJECT!!!!</h1>');
+});
+
+server.use("*", (req, res, next) => {
+  next({ status: 404, message: "Not Found" });
+});
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: `Uh oh: ${err.message}` });
+});
 module.exports = server;
